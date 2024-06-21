3a7551009a9c852c49743d92f14a9528958e358e
Covenant Simon <70116851+ccurves@users.noreply.github.com>
2024-06-21 14:35:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const port = process.env.PORT;
 const paymentRoutes = require("./routes/payments.route");
 
 app.use(express.json());
+
+// Routes
+app.get("/api", (req, res) => {
+  res.send("Gateway API Route");
+});
 app.use("/api/payments", paymentRoutes);
 
 app.listen(port, () => {
